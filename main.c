f7df7c4e2448c0aec9441833cf3b62f2f6ca0190
bauti <jnemi@fie.undef.edu.ar>
2021-11-23 20:55:20 -0300

    ADD - Assignment A2

--- main.c
+++ main.c
@@ -7,7 +7,15 @@
 
 
 int main(){
-    printf("Hello, world!\n");
+    printf("Reading...\n");
+    FILE * file = fopen("A1.txt", "rt");
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char * buffer = (char*) malloc(length + 1);
+    buffer[length] = '\0';
+    fread(buffer, 1, length, file);
+    printf("%s", buffer);
 
     return 0;
 }
