8d8d5dbcc32426fe1bd80c6a297022ffb66e5f6a
bauti <jnemi@fie.undef.edu.ar>
2021-11-24 09:55:19 -0300

    ADD - stack

--- helper.c
+++ helper.c
@@ -3,3 +3,44 @@
 //
 
 #include "helper.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+Stack *newNode() {
+    Stack* aux = (Stack*) malloc(sizeof (Stack));
+    if (aux == NULL) {
+        printf("Not enough memory.\n");
+        exit(-1);
+    }
+    aux->next = NULL;
+    return aux;
+}
+
+void push(Stack *stack, char *string) {
+    Stack* aux = newNode();
+    strcpy((aux->string), string);
+    if (stack == NULL) {
+        stack = aux;
+    } else {
+        aux->next = stack->next;
+        stack = aux;
+    }
+}
+
+char *pop(Stack *stack) {
+    Stack* aux = NULL;
+    char* str = NULL;
+    if (stack != NULL) {
+        aux = stack;
+        stack = aux->next;
+        str = aux->string;
+        free(aux);
+    }
+
+    return str;
+}
+
+
+
--- helper.h
+++ helper.h
@@ -5,4 +5,13 @@
 #ifndef INC_193R_G03_HELPER_H
 #define INC_193R_G03_HELPER_H
 
+typedef struct stack {
+    char* string;
+    struct stack * next;
+} Stack;
+
+Stack* newNode();
+void push(Stack* stack, char* string);
+char* pop(Stack* stack);
+
 #endif //INC_193R_G03_HELPER_H
