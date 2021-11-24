a5ec4a4075a048f67e9da4206f0fff655f2ae418
bauti <jnemi@fie.undef.edu.ar>
2021-11-24 12:29:32 -0300

    ADD - Assignment A4

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "helper.h"
 
 
 int main(){
-    /*
     printf("Reading...\n");
     FILE * file = fopen("A1.txt", "rt");
     fseek(file, 0, SEEK_END);
@@ -17,14 +17,23 @@ int main(){
     char * buffer = (char*) malloc(length + 1);
     buffer[length] = '\0';
     fread(buffer, 1, length, file);
-    printf("%s", buffer);*/
-    Stack*  stack = NULL;
-    push(&stack, "Hello");
-    push(&stack, "world");
-    printf("%s", peak(stack));
-    pop(&stack);
-    printf("%s", peak(stack));
-    pop(&stack);
+    fclose(file);
+    printf("%s", buffer);
+
+    const char separator[2] = "\n\0";
+    Stack* stack = NULL;
+
+    // Assignment
+    char* token = strtok(buffer, separator);
+    while (token != NULL) {
+        push(&stack, token);
+        token = strtok(NULL, separator);
+    }
+
+    while (stack != NULL) {
+        printf("%s", peak(stack));
+        pop(&stack);
+    }
 
     return 0;
 }
