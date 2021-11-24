8d74bcba7c97cdd814b254914d3f4c3d5a6aa7fa
bauti <jnemi@fie.undef.edu.ar>
2021-11-24 17:51:12 -0300

    ADD - Assignment A9

--- main.c
+++ main.c
@@ -4,10 +4,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main(){
-    printf("Hello, world!\n");
+    printf("Opening...\n");
+
+    FILE * file = fopen("A1.txt", "at");
+    char* new_line1 = "New line 1\n";
+    char* new_line2 = "New line 2\n";
+    fwrite(new_line1, 1, strlen(new_line1), file);
+    fwrite(new_line2, 1, strlen(new_line2), file);
+    fclose(file);
+
+
+    printf("Done.\n");
 
     return 0;
 }
