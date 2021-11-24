317be061a123f9d9235a33b8a69b50c3725cc039
bauti <jnemi@fie.undef.edu.ar>
2021-11-24 18:01:23 -0300

    ADD - Assignment A10 bugged

--- main.c
+++ main.c
@@ -7,7 +7,22 @@
 
 
 int main(){
-    printf("Hello, world!\n");
+    printf("Opening...\n");
 
+    // Loading buffer from source file
+    FILE * file = fopen("A1.txt", "rt");
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char* buffer = (char*) malloc(length + 1);
+    fread(buffer, 1, length, file);
+    fclose(file);
+
+    // Cloning into destination file
+    file = fopen("A2.txt", "wt");
+    fwrite(buffer, 1, length, file);
+    fclose(file);
+
+    printf("Done\n");
     return 0;
 }
